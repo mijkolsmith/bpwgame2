40b10421dd37ac6e077f265a2bf62ee1af6accfb
mijkolsmith <mijkolsmith@live.com>
2020-06-11 14:29:12 +0200

    added health and damage

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+	[SerializeField]
+	public List<GameObject> hearts;
+
+	public GameObject deathOverlay;
+
+	private float timer = 0;
+
+	private void Update()
+	{
+		if (timer >= 0)
+		{ timer -= Time.deltaTime; }
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.name == "Enemy1" && timer <= 0 || collision.gameObject.name == "Enemy2Bullet" && timer <= 0 || collision.gameObject.name == "Enemy3Bullet" && timer <= 0)
+		{
+			Destroy(hearts[hearts.Count - 1]);
+			hearts.RemoveAt(hearts.Count - 1);
+			timer = 3f;
+		}
+
+		if (hearts.Count <= 0)
+		{
+			Destroy(gameObject);
+			deathOverlay.SetActive(true);
+		}
+	}
+}

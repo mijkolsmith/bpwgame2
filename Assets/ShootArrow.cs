405835b6907b52bff28e033a288a7bf3984e5f22
mijkolsmith <mijkolsmith@live.com>
2020-06-11 15:20:37 +0200

    backup

---
+++ Assets/ShootArrow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootArrow : MonoBehaviour
+{
+
+	GameObject arrow;
+	float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+		timer -= Time.deltaTime;
+
+		if (timer <= 0)
+		{
+			Instantiate(arrow, new Vector3(1,1,1), Quaternion.identity);
+			timer = 5;
+		}
+    }
+}
